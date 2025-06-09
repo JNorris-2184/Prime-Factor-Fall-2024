55b8b2a7f3fed0408fc6ca9e5f5e7240745b7d15
JDN-Spark <jnorris2575@conestogac.on.ca>
2025-06-09 17:36:10 -0400

    Step 1 testing - invalid data type returns valueError
    Updates:
    1. Minimal code to successfully pass test
    2. conftest.py to enable pytest to work locally
    3. Update to python-app.yml to execute on push to main branch
    4. Remove blank space from _init_.py to resolve linting error

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,12 @@
+"""
+test_generate_prime_factors.py -- Test cases for prime.py
+"""
 
+import pytest
+import prime
+
+
+def test_invalid_datatype():
+    """Assert non-integer raises a ValueError"""
+    with pytest.raises(Exception):
+        prime.generate_prime_factors('hello')
--- prime.py
+++ prime.py
@@ -1,3 +1,13 @@
 """
-prime.py -- Write the application code here
+prime.py -- Application to generate all prime factors for a given number
 """
+
+
+def generate_prime_factors(number):
+    """Return prime factors
+    Args:
+    number (int): any number greater than 0
+    Returns:
+    list: prime factors of number
+    """
+    return int(number)
